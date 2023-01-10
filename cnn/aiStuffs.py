a46b15293817f5f22935f978173d7bf93479ed16
Jairo Sousa <jairo.santos1940@hotmail.com>
2023-01-10 14:29:46 -0300

    U-Net

--- cnn/aiStuffs.py
+++ cnn/aiStuffs.py
@@ -1,125 +1,88 @@
-from tensorflow.keras import Input, Model
-from tensorflow.keras.layers import Layer, Conv2D, Conv2DTranspose, Activation, BatchNormalization, Concatenate, MaxPool2D
-from tensorflow.math import maximum, sign, reduce_sum
-from tensorflow import custom_gradient
+import tensorflow as tf
+from tensorflow.keras import Input, Model, layers
 from tensorflow.keras.callbacks import Callback
 from IPython.display import clear_output
 import matplotlib.pyplot as plt
+from skimage.color import label2rgb
+from random import randint
+import os
 
-@custom_gradient
-def heaviside(x):
-    return 1 - maximum(-sign(x), 0), lambda div: div
-
-def conv_block(x, num_filters):
-    for _ in range(2):
-        x = Conv2D(num_filters, 3, padding='same')(x)
-        x = BatchNormalization()(x)
-        x = Activation('relu')(x)
+def conv_block(x, filters):
+    for lay in (layers.Conv2D(filters, 3, padding='same'),
+                layers.BatchNormalization(),
+                layers.Activation('relu'),
+                layers.Conv2D(filters, 3, padding='same'),
+                layers.BatchNormalization(),
+                layers.Activation('relu')):
+        x = lay(x)
     return x
 
-def encoder_block(x, num_filters):
-    x = conv_block(x, num_filters)
-    p = MaxPool2D((2, 2))(x)
-    return x, p
+def encoder(x, filters):
+    x = conv_block(x, filters)
+    return layers.MaxPool2D((2, 2))(x), x
 
-def decoder_block(x, skip_features, num_filters):
-    x = Conv2DTranspose(num_filters, (2, 2), strides=2, padding='same')(x)
-    x = Concatenate()([x, skip_features])
-    x = conv_block(x, num_filters)
+def decoder(x, jumper, filters):
+    x = layers.Conv2DTranspose(filters, (2, 2), strides=2, padding='same')(x)
+    x = layers.Concatenate()([jumper, x])
+    x = conv_block(x, filters)
     return x
 
-def build_unet(input_shape, u_shape, name='U-Net'):
-    inputs = Input(input_shape)
-    skips = []
-    x = inputs
-    for f in u_shape[:-1]:
-        skip, x = encoder_block(x, f)
-        skips.append(skip)
-        
-    x = conv_block(x, u_shape[-1])
+def build_unet(input_shape:tuple, filters:tuple, name:str, activation:str='sigmoid'):
+    inputs = x = layers.Input(input_shape)
+    jumpers = []
+    for f in filters[:-1]:
+        x, jumper = encoder(x, f)
+        jumpers.append(jumper)
     
-    for f, s in zip(u_shape[:-1][::-1], skips[::-1]):
-        x = decoder_block(x, s, f)
-
-    outputs = Conv2D(1, 1, padding='same', activation=heaviside)(x)
-
-    model = Model(inputs, reduce_sum(outputs, axis=[1, 2]), name=name)
-    return model
-
-class ConvBlock(Layer):
-    def __init__(self, filters):
-        super(ConvBlock, self).__init__()
-        self.convs = [
-            Conv2D(filters, 3, padding='same'),
-            BatchNormalization(),
-            Activation('relu'),
-            Conv2D(filters, 3, padding='same'),
-            BatchNormalization(),
-            Activation('relu')
-        ]
+    x = conv_block(x, filters[-1])
     
-    def call(self, x):
-        for layer in self.convs:
-            x = layer(x)
-        return x
-
-class Encoder(Layer):
-    def __init__(self, filters):
-        super(Encoder, self).__init__()
-        self.conv_block = ConvBlock(filters)
-        self.maxpool = MaxPool2D((2, 2))
-        self.skip = None
+    for f, jumper in zip(filters[::-1][1:], jumpers[::-1]):
+        x = decoder(x, jumper, f)
     
-    def call(self, x):
-        self.skip = self.conv_block(x)
-        return self.maxpool(self.skip)
+    outputs = layers.Conv2D(1, 1, padding='same', activation=activation)(x)
+    return Model(inputs=inputs, outputs=outputs, name=name)
 
-class Decoder(Layer):
-    def __init__(self, filters, encoder):
-        super(Decoder, self).__init__()
-        self.encoder = encoder
-        self.conv_transpose = Conv2DTranspose(filters, (2, 2), strides=2, padding='same')
-        self.concat = Concatenate()
-        self.conv_block = ConvBlock(filters)
-    
-    def call(self, x):
-        return self.conv_block(self.concat([self.encoder.skip, self.conv_transpose(x)]))
-
-class UNetAreaDetection(Model):
-    def __init__(self, filters, activation=heaviside, **kwargs):
-        super(UNetAreaDetection, self).__init__(**kwargs)
-        self.filters = filters
-        self.activation = activation
-
-        contracting = []
-        expanding = []
-        for f in self.filters[:-1]:
-            contracting.append(Encoder(f))
-            expanding.insert(0, Decoder(f, contracting[-1]))
-        
-        self.net_layers = (
-            #[Input(input_shape)] +
-            contracting + # contração
-            [ConvBlock(self.filters[-1])] + # conexão
-            expanding + # expansão
-            [Conv2D(1, 1, padding='same', activation=self.activation)] # ativação
-        )
-    
-    def call(self, x, training):
-        for layer in self.net_layers:
-            x = layer(x)
-        return reduce_sum(x, axis=[1, 2]) if training else x
-
-class TrainingPlot(Callback):
-    def __init__(self, update):
+class UnetTrainingPlot(Callback):
+    def __init__(self, unet, x_test, y_test, period:int=3):
         super(Callback, self).__init__()
-        self.update = update
+
+        self.unet = unet
+        self.x_test = x_test
+        self.y_test = y_test
+        self.period = period
+
+        self.area_total = tf.multiply(*self.y_test.shape[1:-1]).numpy()
+        self.y_rel_area = tf.reduce_sum(self.y_test, (1, 2))/self.area_total
+
+        ymin, ymax = tf.reduce_min(self.y_rel_area), tf.reduce_max(self.y_rel_area)
+        dy = 0.2*(ymax - ymin)
+        self.t_min, self.t_max = ymin - dy, ymax + dy
+        self.t = tf.linspace(self.t_min, self.t_max, 50)
+
         self.logs = {}
     
     def on_epoch_end(self, epoch, logs={}):
         for k, v in logs.items():
             try: self.logs[k].append(v)
             except KeyError: self.logs[k] = [v]
-        clear_output(wait=True)
-        self.update(self.logs, self.model)
-        plt.show()
+        
+        if not epoch % self.period:
+            pred = tf.where(self.unet.predict(self.x_test, verbose=0) > 0.5, 1, 0)
+            pred_rel_area = tf.reduce_sum(pred, axis=(1, 2))/self.area_total
+
+            clear_output(wait=True)
+            fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(20, 4))
+
+            for k, v in self.logs:
+                ax1.plot(v, label=k)
+            ax1.legend()
+            ax1.set_ylim(bottom=0)
+
+            i = randint(0, self.x_test.shape[0]-1)
+            ax2.imshow(label2rgb(pred[i, :, :, 0], self.x_test[i, :, :, 0], bg_label=0))
+
+            ax3.plot(self.pred_rel_area, rel_area, 'ro', alpha=0.5)
+            ax3.plot(self.t, self.t, 'k--')
+            ax3.set_ylim(self.t_min(), self.t_max())
+            ax3.set_aspect('equal')
+            plt.show()
