74144389d6dfdc908b7d964a01c7f46a2632f11c
Jairo Sousa <jairo.santos1940@hotmail.com>
2023-03-10 10:22:56 -0300

    Squashed commit of the following:
    commit 7edf9bff77952cf2aab3e07006e7d190b0662b05
    Author: Jairo Sousa <jairo.santos1940@hotmail.com>
    Date:   Fri Mar 10 10:20:16 2023 -0300
    
        Squashed commit of the following:
    
        commit 22d9c538c2358f85a18961f861f8959b23f229d9
        Author: root <jairo.santos1940@hotmail.com>
        Date:   Fri Mar 10 20:33:08 2023 +0000
    
            Dice + Topk
    
        commit 9240000d2aa6f6a3185abc72eb6a78d4bfa45fdf
        Author: Jairo Sousa <jairo.santos1940@hotmail.com>
        Date:   Thu Mar 9 09:30:24 2023 -0300
    
            Squashed commit of the following:
    
            commit a63def1abe57f8f758e5d40bb12b07b1397abb58
            Author: Jairo Sousa <jairo.santos1940@hotmail.com>
            Date:   Thu Mar 9 04:56:44 2023 -0300
    
                Implementando função para padronizar dados de treinamento
    
            commit 5e7574259235542b659d3e4cd40f94f74c084ca9
            Author: Jairo Sousa <jairo.santos1940@hotmail.com>
            Date:   Thu Mar 9 01:50:50 2023 -0300
    
                Organizando diretórios
    
            commit 727ca58c054bd609d35c1bf04aa072afa309850d
            Merge: 48127f0 8f05d69
            Author: Jairo Sousa <jairo.santos1940@hotmail.com>
            Date:   Thu Mar 9 01:44:58 2023 -0300
    
                Merge branch 'losses' into structure
    
            commit 48127f0a8b492605788a3130e409c1d7af1301a4
            Author: Jairo Sousa <jairo.santos1940@hotmail.com>
            Date:   Wed Mar 8 11:46:54 2023 -0300
    
                CameraPage e InfoPage
    
            commit b2bdbabfabde86239f8ac85b0de6a9ff731704ee
            Author: Jairo Sousa <jairo.santos1940@hotmail.com>
            Date:   Tue Mar 7 23:12:30 2023 -0300
    
                criando app
    
    commit e4990c9e1e058fc783cd0a8418b1b879bf670540
    Author: Jairo Sousa <jairo.santos1940@hotmail.com>
    Date:   Thu Mar 9 14:31:23 2023 -0300
    
        Funções para adicionar amostras ao dataset e verificar a integridade dos dados
    
    commit a63def1abe57f8f758e5d40bb12b07b1397abb58
    Author: Jairo Sousa <jairo.santos1940@hotmail.com>
    Date:   Thu Mar 9 04:56:44 2023 -0300
    
        Implementando função para padronizar dados de treinamento
    
    commit 5e7574259235542b659d3e4cd40f94f74c084ca9
    Author: Jairo Sousa <jairo.santos1940@hotmail.com>
    Date:   Thu Mar 9 01:50:50 2023 -0300
    
        Organizando diretórios
    
    commit 727ca58c054bd609d35c1bf04aa072afa309850d
    Merge: 48127f0 8f05d69
    Author: Jairo Sousa <jairo.santos1940@hotmail.com>
    Date:   Thu Mar 9 01:44:58 2023 -0300
    
        Merge branch 'losses' into structure
    
    commit 8f05d698bb21628cb26004a3fa7e0d33f0c35672
    Author: root <jairo.santos1940@hotmail.com>
    Date:   Thu Mar 9 01:29:10 2023 +0000
    
        Implementando e testando métricas e funções de perda para treinar modelos de segmentação
    
        Impementado as métricas IoU e DSC. Implementando e testando as funções de perda Dice Loss e Top-k Loss.
    
    commit 264c785d7d5a9468ce47fd2e217fc42fa6edf645
    Merge: 3662177 8aa55ed
    Author: Jairo Sousa <jairo.santos1940@hotmail.com>
    Date:   Wed Mar 1 15:14:34 2023 -0300
    
        Merge branch 'structure' into losses
    
    commit 36621772bbfdd01da72681e307ca80dca51bb105
    Author: root <jairo.santos1940@hotmail.com>
    Date:   Sun Feb 26 00:31:52 2023 +0000
    
        Organizando funções e classes
    
    commit c366ada2f3de567630e6b3f78e84de3611f51829
    Author: root <jairo.santos1940@hotmail.com>
    Date:   Sat Feb 25 23:30:28 2023 +0000
    
        Adicionando TopK Loss function e analisando Boundary-based metrics

--- src/visualize.py
+++ src/visualize.py
@@ -14,62 +14,63 @@ def plot_image(image, ax=None, **kwargs):
 def plot_label(image, label, **kwargs):
     return plot_image(label2rgb(np.squeeze(label), np.squeeze(image), bg_label=0), **kwargs)
 
+def plot_seg_contour(image, label, y_pred, th=0.5, ax=None):
+    ax = (ax if ax is not None else plt)
+    plot_label(image, y_pred >= th, ax=ax)
+    ax.contour(y_pred, levels=(0, 0.25, 0.5, 0.75, 1), cmap='magma')
+    ax.contour(label, cmap='bone')
+
 def set_custom_style():
     use({
         'scatter.edgecolors':'black',
         'lines.linewidth':2
     })
 
-def plot_training(model, clear=False, ranking=False):
+def plot_training(unet, clear=False, ranking=False):
 
     # ==================== Model info ====================
-    y_pred_test = model.predict(model.x_test)
-    y_pred_train = model.predict(model.x_train)
+    y_pred_test = unet.model.predict(unet.x_test, verbose=0)
+    y_pred_train = unet.model.predict(unet.x_train, verbose=0)
     area_pred_test = np.mean(y_pred_test, axis=(-1, -2, -3))
     area_pred_train = np.mean(y_pred_train, axis=(-1, -2, -3))
 
     j = np.random.randint(len(y_pred_test))
-    img = model.x_test[j, ..., 0]
+    img = unet.x_test[j, ..., 0]
+    gtruth = unet.y_test[j, ..., 0]
     pred = y_pred_test[j, ..., 0]
-    H, W = img.shape
 
-    logs = model.get_logs()
+    logs = unet.get_logs()
     best_epoch = logs.epoch[np.argmin(logs.val_loss)]
-    n_metrics = len(model.metrics) - 1
+    n_metrics = len(unet.model.metrics) - 1
 
     # ==================== Figure config ====================
-    fig = plt.figure(figsize=(20, 10))
-    main_grid = GridSpec(3, 4, figure=fig)
-    metrics_grid = main_grid[-1, :].subgridspec(1, n_metrics)
+    fig = plt.figure(figsize=(18, 7))
+    main_grid = GridSpec(10, 4, figure=fig)
+    mh = 4
+    metrics_grid = main_grid[-mh:, :].subgridspec(1, n_metrics)
 
     axs = {
-        'seg': fig.add_subplot(main_grid[:-1, 0]),
-        'prec': fig.add_subplot(main_grid[:-1, -1]),
-        'loss': fig.add_subplot(main_grid[:-1, 1:-1]),
+        'seg': fig.add_subplot(main_grid[:-mh, 0]),
+        'prec': fig.add_subplot(main_grid[:-mh, -1]),
+        'loss': fig.add_subplot(main_grid[:-mh, 1:-1]),
     }
 
     # ==================== Segmentation ====================
-    plot_label(img, pred >= 0.5, ax=axs['seg'])
-    axs['seg'].contour(
-        *np.meshgrid(np.arange(W), np.arange(H)), 
-        pred,
-        levels= (0, 0.25, 0.5, 0.75, 1), 
-        cmap='magma'
-    )
+    plot_seg_contour(img, gtruth, pred, 0.5, axs['seg'])
 
     # ==================== Loss ====================
     axs['loss'].plot(logs.epoch, logs.loss, label='loss')
     axs['loss'].plot(logs.epoch, logs.val_loss, label='validation loss')
-    axs['loss'].vlines(best_epoch, label=f'best epoch: {best_epoch}', linestyle='dashed', color='k')
+    axs['loss'].vlines(best_epoch, *axs['loss'].get_ylim(), label=f'best epoch: {best_epoch}', linestyle='dashed', color='k')
     axs['loss'].set_xlabel('epoch')
     axs['loss'].set_ylabel('loss')
     axs['loss'].semilogy()
-    axs['loss'].grid(True)
+    axs['loss'].grid(True, axis='y')
     axs['loss'].legend()
 
     # ==================== Precision ====================
-    axs['prec'].scatter(np.mean(model.y_train, axis=(-1, -2, -3)), area_pred_train, alpha=0.7, label='training data')
-    axs['prec'].scatter(np.mean(model.y_test, axis=(-1, -2, -3)), area_pred_test, alpha=0.7, label='validation data')
+    axs['prec'].scatter(np.mean(unet.y_train, axis=(-1, -2, -3)), area_pred_train, alpha=0.7, label='training data')
+    axs['prec'].scatter(np.mean(unet.y_test, axis=(-1, -2, -3)), area_pred_test, alpha=0.7, label='validation data')
     axs['prec'].set_aspect('equal')
     xmin, xmax = axs['prec'].get_xlim()
     dx = (xmax - xmin)*0.1
@@ -80,12 +81,13 @@ def plot_training(model, clear=False, ranking=False):
     axs['prec'].set_ylim(xmin, xmax)
     axs['prec'].set_xlabel('true rel area')
     axs['prec'].set_ylabel('pred rel area')
+    axs['prec'].legend()
     
     # ==================== Metrics ====================
-    for i, metric in enumerate(model.metrics):
+    for i, metric in enumerate(unet.model.metrics[1:]):
         ax = fig.add_subplot(metrics_grid[0, i])
-        ax.plot(logs.epoch, logs[metric.name], label=metric.name)
-        ax.plot(logs.epoch, logs[f'val_{metric.name}'], label=f'validation {metric.name}')
+        ax.plot(logs.epoch, logs[metric.name], label='training data')
+        ax.plot(logs.epoch, logs[f'val_{metric.name}'], label=f'validation data')
         ax.set_xlabel('epoch')
         ax.set_ylabel(metric.name)
         ax.grid(True)
@@ -98,15 +100,15 @@ def plot_training(model, clear=False, ranking=False):
     plt.show()
 
     if ranking != False: 
-        display(HTML(logs.sort_values('val_loss', ascending=False).head(int(ranking)).to_html()))
+        display(HTML(logs.sort_values('val_loss', ascending=True).head(int(ranking)).to_html()))
 
 class TrainingBoard(Callback):
-    def __init__(self, model, period, ranking):
+    def __init__(self, unet, period, ranking):
         super().__init__()
-        self.model = model
+        self.unet = unet
         self.period = period
         self.ranking = ranking
     
     def on_epoch_end(self, epoch, logs=None):
         if epoch%self.period == 0:
-            plot_training(self.model, clear=True, ranking=self.ranking)
+            plot_training(self.unet, clear=True, ranking=self.ranking)
