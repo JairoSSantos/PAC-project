4c247e51e541ba8af552d753852f46822ac79b12
Jairo Sousa <jairo.santos1940@hotmail.com>
2023-03-11 18:30:19 -0300

    Implementando ajuste de foco e delimitando espaço da amostra na imagem

--- pac_app/lib/pages/camera.dart
+++ pac_app/lib/pages/camera.dart
@@ -12,18 +12,47 @@ class CameraPage extends StatefulWidget {
 }
 
 class _CameraPageState extends State<CameraPage> {
+  bool showFocusCircle = false;
+  double x = 0;
+  double y = 0;
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(title: const Text('Pellet Area Calculator')),
-      body: Center(child: CameraPreview(widget.controller)),
+      body: Center(
+        child: Stack(
+          children: <Widget>[
+            GestureDetector(
+              onTapUp: (details) => _onTap(details),
+              child: Stack(
+                children: <Widget>[
+                  Center(child: CameraPreview(widget.controller)),
+                  if(showFocusCircle) Positioned(
+                      top: y-20,
+                      left: x-20,
+                      child: Container(
+                        height: 40,
+                        width: 40,
+                        decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(color: Colors.white,width: 1.5)
+                    ),
+                  ))
+                ]
+              )),
+            CustomPaint(painter: RectPainter(x: size.width/2, y: 250))
+          ]
+        )
+      ),
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
           const Spacer(flex:3),
           FloatingActionButton(
             backgroundColor: Colors.white,
+            foregroundColor: Colors.grey,
             onPressed: () {
               widget.controller.resumePreview();
               Navigator.push(
@@ -32,13 +61,14 @@ class _CameraPageState extends State<CameraPage> {
               );
             },
             heroTag: 'camera',
+            child: const Icon(Icons.camera_alt_outlined)
           ),
           const Spacer(flex:1),
           FloatingActionButton(
             backgroundColor: Colors.transparent,
             onPressed: (){},
             heroTag: 'gellery',
-            child: const Icon(Icons.add_photo_alternate),
+            child: const Icon(Icons.photo),
           ),
           const Spacer(flex:1)
         ]
@@ -47,4 +77,54 @@ class _CameraPageState extends State<CameraPage> {
       backgroundColor: Colors.blue,
     );
   }
+
+  Future<void> _onTap(TapUpDetails details) async {
+    if(widget.controller.value.isInitialized) {
+      showFocusCircle = true;
+      x = details.localPosition.dx;
+      y = details.localPosition.dy;
+
+      double fullWidth = MediaQuery.of(context).size.width;
+      double cameraHeight = fullWidth * widget.controller.value.aspectRatio;
+
+      double xp = x / fullWidth;
+      double yp = y / cameraHeight;
+
+      Offset point = Offset(xp,yp);
+      debugPrint("point : $point");
+
+      // Manually focus
+      await widget.controller.setFocusPoint(point);
+      
+      // Manually set light exposure
+      //controller.setExposurePoint(point);
+      
+      setState(() {
+        Future.delayed(const Duration(seconds: 2)).whenComplete(() {
+          setState(() {
+            showFocusCircle = false;
+          });
+        });
+      });
+    }
+  }
+}
+
+class RectPainter extends CustomPainter {
+  final double x, y;
+
+  const RectPainter({required this.x, required this.y});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint0 = Paint()
+      ..color = Colors.red
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 3.0;
+    //a rectangle
+    canvas.drawRect(Rect.fromCenter(center: Offset(x, y), width: 256, height: 256), paint0);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
 }
